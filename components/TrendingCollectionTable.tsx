f45ce004d2042443025a3118f844bf2aec29bbec
workhorse88 <gsolutions878@gmail.com>
2023-02-13 11:03:42 +0200

    fix: make the whole collection row clickable

--- components/TrendingCollectionTable.tsx
+++ components/TrendingCollectionTable.tsx
@@ -88,85 +88,89 @@ const TrendingCollectionTable: FC<Props> = ({ fallback }) => {
             }
 
             return (
-              <tr
+              <Link
+                href={tokenHref}
+                legacyBehavior={true}
+                passHref
                 key={`${contract}-${index}`}
-                ref={
-                  index === arr.length - 5 && shouldInfiniteLoad ? ref : null
-                }
-                className={`${
-                  index === arr.length - 1 ||
-                  (FOOTER_ENABLED && !expanded && index == 9)
-                    ? ''
-                    : 'border-b'
-                } group h-[88px] border-neutral-300 dark:border-neutral-600 dark:text-white`}
               >
-                {/* COLLECTION */}
-                <td className="reservoir-body flex items-center gap-4 whitespace-nowrap px-6 py-4 dark:text-white">
-                  <div className="reservoir-h6 mr-6 dark:text-white">
-                    {index + 1}
-                  </div>
-                  <Link href={tokenHref} legacyBehavior={true}>
-                    <a className="flex items-center gap-2">
-                      <img
-                        src={optimizeImage(image, 140)}
-                        className="h-[56px] w-[56px] rounded-full object-cover"
-                      />
-                      <div
-                        className={`reservoir-h6 overflow-hidden truncate whitespace-nowrap dark:text-white ${
-                          isSmallDevice ? 'max-w-[140px]' : ''
-                        }`}
-                      >
-                        {name}
-                      </div>
-                    </a>
-                  </Link>
-                </td>
+                <tr
+                  ref={
+                    index === arr.length - 5 && shouldInfiniteLoad ? ref : null
+                  }
+                  className={`${
+                    index === arr.length - 1 ||
+                    (FOOTER_ENABLED && !expanded && index == 9)
+                      ? ''
+                      : 'border-b'
+                  } group h-[88px] border-neutral-300 dark:border-neutral-600 dark:text-white cursor-pointer`}
+                >
+                  {/* COLLECTION */}
+                  <td className="reservoir-body flex items-center gap-4 whitespace-nowrap px-6 py-4 dark:text-white">
+                    <div className="reservoir-h6 mr-6 dark:text-white">
+                      {index + 1}
+                    </div>
+                      <a className="flex items-center gap-2">
+                        <img
+                          src={optimizeImage(image, 140)}
+                          className="h-[56px] w-[56px] rounded-full object-cover"
+                        />
+                        <div
+                          className={`reservoir-h6 overflow-hidden truncate whitespace-nowrap dark:text-white ${
+                            isSmallDevice ? 'max-w-[140px]' : ''
+                          }`}
+                        >
+                          {name}
+                        </div>
+                      </a>
+                  </td>
 
-                {/* VOLUME */}
-                {!isSmallDevice && (
+                  {/* VOLUME */}
+                  {!isSmallDevice && (
+                    <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
+                      <FormatNativeCrypto
+                        amount={
+                          sort === '7DayVolume'
+                            ? days7
+                            : sort === '30DayVolume'
+                            ? days30
+                            : days1
+                        }
+                      />
+                      <PercentageChange
+                        value={
+                          sort === '7DayVolume'
+                            ? days7Change
+                            : sort === '30DayVolume'
+                            ? days30Change
+                            : days1Change
+                        }
+                      />
+                    </td>
+                  )}
+
+                  {/* FLOOR PRICE */}
                   <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
-                    <FormatNativeCrypto
-                      amount={
-                        sort === '7DayVolume'
-                          ? days7
-                          : sort === '30DayVolume'
-                          ? days30
-                          : days1
-                      }
-                    />
+                    <FormatNativeCrypto amount={floorPrice} />
                     <PercentageChange
                       value={
                         sort === '7DayVolume'
-                          ? days7Change
+                          ? floorSaleChange7Days
                           : sort === '30DayVolume'
-                          ? days30Change
-                          : days1Change
+                          ? floorSaleChange30Days
+                          : floorSaleChange1Days
                       }
                     />
                   </td>
-                )}
 
-                {/* FLOOR PRICE */}
-                <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
-                  <FormatNativeCrypto amount={floorPrice} />
-                  <PercentageChange
-                    value={
-                      sort === '7DayVolume'
-                        ? floorSaleChange7Days
-                        : sort === '30DayVolume'
-                        ? floorSaleChange30Days
-                        : floorSaleChange1Days
-                    }
-                  />
-                </td>
-
-                {/* SUPPLY */}
-                {!isSmallDevice && (
-                  <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
-                    {supply ? formatNumber(+supply) : '-'}
-                  </td>
-                )}
-              </tr>
+                  {/* SUPPLY */}
+                  {!isSmallDevice && (
+                    <td className="reservoir-body whitespace-nowrap px-6 py-4 dark:text-white">
+                      {supply ? formatNumber(+supply) : '-'}
+                    </td>
+                  )}
+                </tr>
+              </Link>
             )
           })}
         </tbody>
