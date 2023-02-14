e897e2934c708fa9dbca0b86636b57c7468e0772
workhorse88 <gsolutions878@gmail.com>
2023-02-14 09:11:49 +0200

    fix: ProductsDropdown fix styles, links and icons

--- components/ProductsDropdown.tsx
+++ components/ProductsDropdown.tsx
@@ -24,12 +24,12 @@ const ProductsDropdown: FC = () => {
       <DropdownMenu.Content align="center" sideOffset={6}>
         <div
           style={{boxShadow: '0px 4px 32px rgba(0, 0, 0, 0.16)'}}
-          className={`w-48 space-y-1  bg-white px-4 py-6 radix-side-bottom:animate-slide-down dark:bg-neutral-900 md:w-[320px] rounded-[12px]`}
+          className={`w-48 space-y-1  bg-white px-3 py-3 radix-side-bottom:animate-slide-down dark:bg-neutral-900 md:w-[320px] rounded-[12px]`}
         >
           {/* Marketplace */}
-          <Link href={`/address/`} legacyBehavior={true}>
+          <Link href={`/`} legacyBehavior={true}>
             <DropdownMenu.Item asChild>
-              <div className="group flex w-full items-center gap-6 px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
+              <div className="group flex w-full items-center gap-2 px-2 py-2 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
                 <Avatar address={''} avatar={'/icons/main-nav/marketplace.svg'} size={40} />
                 <div>
                   <p className='text-[14px] leading-6'>Marketplace</p>
@@ -40,9 +40,9 @@ const ProductsDropdown: FC = () => {
           </Link>
 
           {/* NFT Embed */}
-          <Link href={`/address/`} legacyBehavior={true}>
+          <a href='https://www.nftembed.org/' target="_blank" rel="noreferrer">
             <DropdownMenu.Item asChild>
-              <div className="group flex w-full items-center gap-6 px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
+              <div className="group flex w-full items-center gap-2 px-2 py-2 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
                 <Avatar address={''} avatar={'/icons/main-nav/nft-embed.svg'} size={40} />
                 <div>
                   <p className='text-[14px] leading-6'>NFT Embed</p>
@@ -50,42 +50,42 @@ const ProductsDropdown: FC = () => {
                 </div>
             </div>
             </DropdownMenu.Item>
-          </Link>
-
-          {/* Minting */}
-          <Link href={`/address/`} legacyBehavior={true}>
-            <DropdownMenu.Item asChild>
-              <div className="group flex w-full items-center gap-6 px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
-                <Avatar address={''} avatar={'/icons/main-nav/minting.svg'} size={40} />
-                <div>
-                  <p className='text-[14px] leading-6'>Minting</p>
-                  <p className='text-[12px] leading-4 text-[#00000066]'>Create NFTs & collections</p>
-                </div>
-            </div>
-            </DropdownMenu.Item>
-          </Link>
+          </a>
 
           {/* NFT Torrent */}
-          <Link href={`/address/`} legacyBehavior={true}>
+          <a href='https://graviton.xyz/products/torrent' target="_blank" rel="noreferrer">
             <DropdownMenu.Item asChild>
-              <div className="group flex w-full items-center gap-6 px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
+              <div className="group flex w-full items-center gap-2 px-2 py-2 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
                 <Avatar address={''} avatar={'/icons/main-nav/nft-torrent.svg'} size={40} />
                 <div>
                   <p className='text-[14px] leading-6'>NFT Torrent</p>
-                  <p className='text-[12px] leading-4 text-[#00000066]'>Safely seed and store your files</p>
+                  <p className='text-[12px] leading-4 text-[#00000066]'>Safely mint,seed and store your files</p>
                 </div>
             </div>
             </DropdownMenu.Item>
-          </Link>
+          </a>
 
           {/* NFT Display */}
-          <Link href={`/address/`} legacyBehavior={true}>
+          <a href='https://graviton.xyz/products/display' target="_blank" rel="noreferrer">
             <DropdownMenu.Item asChild>
-              <div className="group flex w-full items-center gap-6 px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
+              <div className="group flex w-full items-center gap-2 px-2 py-2 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
                 <Avatar address={''} avatar={'/icons/main-nav/nft-display.svg'} size={40} />
                 <div>
                   <p className='text-[14px] leading-6'>NFT Display</p>
-                  <p className='text-[12px] leading-4 text-[#00000066]'>Showcase your favourites on any screen</p>
+                  <p className='text-[12px] leading-4 text-[#00000066]'>Showcase your favorites on any screen</p>
+                </div>
+            </div>
+            </DropdownMenu.Item>
+          </a>
+
+          {/* Metaversian Republic */}
+          <Link href={`/`} legacyBehavior={true}>
+            <DropdownMenu.Item asChild>
+              <div className="group flex w-full items-center gap-2 px-2 py-2 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[12px]">
+                <Avatar address={''} avatar={'/icons/main-nav/metaverse-republic.svg'} size={40} />
+                <div>
+                  <p className='text-[14px] leading-6'>Metaversian Republic</p>
+                  <p className='text-[12px] leading-4 text-[#00000066]'>Polymorph shooter game</p>
                 </div>
             </div>
             </DropdownMenu.Item>
