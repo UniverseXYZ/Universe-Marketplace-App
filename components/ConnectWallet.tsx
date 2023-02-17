fef0b391181ec258cffdf9830c6f5d6568c32c2c
workhorse88 <gsolutions878@gmail.com>
2023-02-17 10:43:15 +0200

    feat: update main nav styles and footer

--- components/ConnectWallet.tsx
+++ components/ConnectWallet.tsx
@@ -43,34 +43,43 @@ const ConnectWallet: FC = () => {
 
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger className="btn-primary-outline ml-auto rounded-full border-transparent p-0 normal-case dark:border-neutral-600 dark:bg-neutral-900 dark:ring-primary-900 dark:focus:ring-4">
+      <DropdownMenu.Trigger className="flex items-center ml-auto rounded-full focus-visible:outline-none border-transparent p-0 normal-case">
         <Avatar address={account.address} avatar={ensAvatar} size={40} />
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Content align="end" sideOffset={6}>
         <div
-          className={`w-48 space-y-1  bg-white px-1.5 py-2 shadow-md radix-side-bottom:animate-slide-down dark:bg-neutral-900 md:w-56 ${
-            DISABLE_POWERED_BY_RESERVOIR ? 'rounded' : 'rounded-t'
-          }`}
+          style={{boxShadow: '0px 4px 32px rgba(0, 0, 0, 0.16)'}}
+          className="w-[240px] space-y-1 bg-white px-3 py-2 shadow-md radix-side-bottom:animate-slide-down dark:bg-neutral-900 md:w-56 rounded-[8px]"
         >
-          <div className="group flex w-full items-center justify-between rounded px-4 py-3 outline-none transition">
-            {ensName ? (
-              <span>{truncateEns(ensName)}</span>
-            ) : (
-              <span>{truncateAddress(account.address || '')}</span>
-            )}
+          {/* Balance and ENS */}
+          <div className='border rounded-lg mb-[20px]'>
+            <div className="group flex w-full items-center justify-between rounded px-4 py-2 outline-none transition text-base font-semibold">
+              {ensName ? (
+                <span>{truncateEns(ensName)}</span>
+              ) : (
+                <span>{truncateAddress(account.address || '')}</span>
+              )}
+            </div>
+            <div className="group flex w-full items-center justify-between rounded px-4 py-2 outline-none transition text-sm font-semibold">
+              <span>
+                {account.address && <Balance address={account.address} />}
+              </span>
+            </div>
           </div>
-          <div className="group flex w-full items-center justify-between rounded px-4 py-3 outline-none transition">
-            <span>Balance </span>
-            <span>
-              {account.address && <Balance address={account.address} />}
-            </span>
-          </div>
+
           <Link href={`/address/${account.address}`} legacyBehavior={true}>
             <DropdownMenu.Item asChild>
-              <a className="group flex w-full cursor-pointer items-center justify-between rounded px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800">
-                Portfolio
-              </a>
+              <div className="group flex w-full items-center gap-3 px-3 py-2 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800 cursor-pointer rounded-[8px]">
+                <img
+                  className="object-fit"
+                  src='/icons/main-nav/profile.svg'
+                  alt={'rarity'}
+                  width='24px'
+                  height='24px'
+                />
+                <p className='text-[14px] leading-6 font-medium'>Portfolio</p>
+                </div>
             </DropdownMenu.Item>
           </Link>
           <DropdownMenu.Item asChild>
@@ -79,10 +88,16 @@ const ConnectWallet: FC = () => {
               onClick={() => {
                 disconnect()
               }}
-              className="group flex w-full cursor-pointer items-center justify-between gap-3 rounded px-4 py-3 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+              className="group flex w-full cursor-pointer items-center gap-3 rounded-[8px] px-3 py-2 outline-none transition hover:bg-neutral-100 focus:bg-neutral-100 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
             >
-              <span>Disconnect</span>
-              <HiOutlineLogout className="h-6 w-7" />
+              <img
+                className="object-fit"
+                src='/icons/main-nav/sign-out.svg'
+                alt={'rarity'}
+                width='24px'
+                height='24px'
+              />
+              <p className='text-[14px] leading-6 font-medium'>Disconnect</p>
             </button>
           </DropdownMenu.Item>
         </div>
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,39 +7,42 @@ const Footer = () => {
   if (FOOTER_ENABLED)
     return (
       <footer className="col-span-full flex flex-col items-center justify-between px-8 pb-8 pt-8 sm:flex-row md:px-16 border-t mt-12 border-[#0000001a]">
-        <div className="mb-6 flex flex-row flex-wrap items-center justify-between gap-x-6 text-xs sm:mb-0 sm:gap-x-8 sm:text-sm">
+        {/* Links */}
+        <div className="mb-6 flex flex-row flex-wrap items-center justify-between gap-x-6 text-xs sm:mb-0 sm:gap-x-8 sm:text-sm text-[#00000099]">
           <span>
             Universe.xyz © 2023. Open-sourced.
           </span>
           <Link href="https://app.sushi.com/legacy/add/0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48/0x618679df9efcd19694bb1daa8d00718eacfa2883?chainId=1" legacyBehavior={true}>
-            <a className="" target="_blank" rel="noreferrer">
+            <a className="hover:text-[#000000]" target="_blank" rel="noreferrer">
               Add liquidity to SushiSwap USDC/XYZ pool
             </a>
           </Link>
           <Link href="https://app.sushi.com/swap?inputCurrency=0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48&outputCurrency=0x618679dF9EfCd19694BB1daa8D00718Eacfa2883&chainId=1" legacyBehavior={true}>
-            <a className="min-w-max" target="_blank" rel="noreferrer">
+            <a className="min-w-max hover:text-[#000000]" target="_blank" rel="noreferrer">
               SushiSwap USDC/XYZ market
             </a>
           </Link>
         </div>
+
+        {/* Social */}
         <div className="flex flex-row items-center gap-x-6">
           <Link href="https://twitter.com/universe_xyz" legacyBehavior={true}>
-            <a className="" target="_blank" rel="noreferrer">
+            <a className="flex items-center justify-center w-[40px] h-[40px] rounded-lg bg-[#00000008] hover:bg-[#0000001a]" target="_blank" rel="noreferrer">
               <FaTwitter className="h-[20px] w-[25px]" />
             </a>
           </Link>
           <Link href="https://discord.com/invite/vau77wXCD3" className="ml-5" legacyBehavior={true}>
-            <a className="" target="_blank" rel="noreferrer">
+            <a className="flex items-center justify-center w-[40px] h-[40px] rounded-lg bg-[#00000008] hover:bg-[#0000001a]" target="_blank" rel="noreferrer">
               <FaDiscord className="h-[19px] w-[25px]" />
             </a>
           </Link>
           <Link href="https://www.youtube.com/channel/UCWt00md9T2b4iTsHWp_Fapw" className="ml-5" legacyBehavior={true}>
-            <a className="" target="_blank" rel="noreferrer">
+            <a className="flex items-center justify-center w-[40px] h-[40px] rounded-lg bg-[#00000008] hover:bg-[#0000001a]" target="_blank" rel="noreferrer">
               <FaYoutube className="h-[19px] w-[25px]" />
             </a>
           </Link>
           <Link href="https://medium.com/universe-xyz" className="ml-5" legacyBehavior={true}>
-            <a className="" target="_blank" rel="noreferrer">
+            <a className="flex items-center justify-center w-[40px] h-[40px] rounded-lg bg-[#00000008] hover:bg-[#0000001a]" target="_blank" rel="noreferrer">
               <FaMediumM className="h-[19px] w-[25px]" />
             </a>
           </Link>
