e4140c22984c45d7ad4f14e50d748eef3b62ff8c
workhorse88 <gsolutions878@gmail.com>
2023-02-17 11:38:49 +0200

    feat: stick the footer to the bottom

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,18 +11,18 @@ type Props = {
 
 const Layout: FC<Props> = ({ children, navbar }) => {
   return (
-    <>
+    <div className='h-screen flex flex-col justify-between'>
       <Toaster
         position={'top-right'}
         containerStyle={{ zIndex: 100000000000 }}
       />
       <NetworkWarning />
-      <main className="mx-auto grid max-w-[2560px] grid-cols-4 gap-x-4 pb-4 md:grid-cols-8 lg:grid-cols-12 3xl:grid-cols-16 4xl:grid-cols-21">
+      <main className="min-[2560px]:mx-auto min-[2560px]:min-w-[2560px] grid max-w-[2560px] grid-cols-4 gap-x-4 pb-4 md:grid-cols-8 lg:grid-cols-12 3xl:grid-cols-16 4xl:grid-cols-21">
         <Navbar {...navbar} />
         {children}
       </main>
       <Footer/>
-    </>
+    </div>
   )
 }
 
